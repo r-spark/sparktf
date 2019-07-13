c61637d145c2d7fabf2660d3e06bae02ac6cc6bc
HarryZhu <7harryprince@gmail.com>
2019-07-13 23:41:57 +0800

    Update inference.R

--- R/inference.R
+++ R/inference.R
@@ -1,1 +1,14 @@
 
+
+ml_load_tf <- function(mdl_dir, input_mapping, output_mapping, ...){
+  sparklyr::invoke_new(sc,"com.yahoo.tensorflowonspark.TFModel") %>%
+    sparklyr::invoke(method="setModel",mdl_dir) %>%
+    sparklyr::invoke(method="setInputMapping",input_mapping) %>%
+    sparklyr::invoke(method="setOutputMapping",output_mapping)
+}
+
+
+
+ml_predict_tf <- function(x , dataset, ...){
+  sparklyr::invoke(x, method = "transform", dataset)
+}
