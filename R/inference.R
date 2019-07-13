faedb0b0da52cb90df0feab13a17fd786ac37594
HarryZhu <7harryprince@gmail.com>
2019-07-14 00:35:33 +0800

    Update inference.R

--- R/inference.R
+++ R/inference.R
@@ -1,10 +1,14 @@
 
 
 ml_load_tf <- function(mdl_dir, input_mapping, output_mapping, ...){
+  im <- invoke_static(sc,"sparktf.StringUtils","StringToMap",input_mapping)
+  om <- invoke_static(sc,"sparktf.StringUtils","StringToMap",output_mapping)
+  
+  
   sparklyr::invoke_new(sc,"com.yahoo.tensorflowonspark.TFModel") %>%
     sparklyr::invoke(method="setModel",mdl_dir) %>%
-    sparklyr::invoke(method="setInputMapping",input_mapping) %>%
-    sparklyr::invoke(method="setOutputMapping",output_mapping)
+    sparklyr::invoke(method="setInputMapping",im) %>%
+    sparklyr::invoke(method="setOutputMapping",om)
 }
 
 
